b2d7b5fc7650dbb7d1df4cbb4e99b90509e06382
VWKafer <noah104.meier@gmail.com>
2026-09-18 21:12:40 +0200

    faliure

--- Assets/scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/scripts/PlayerScripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
@@ -21,8 +21,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update(){
         _moveDirection = Move.action.ReadValue<Vector3>();
-        _facingDirection = Turn.action.ReadValue<Vector3>();
-        Debug.WriteLine("_facingDirection.x");
+        _facingDirection = Turn.action.ReadValue<Vector2>();
+        Debug.Log("Hello: " + _facingDirection.y);
+        Cameratrans.localRotation = Quaternion.Euler( _facingDirection.y*moussensetivity, 0,0); 
+        transform.localRotation = Quaternion.Euler(0, _facingDirection.x*moussensetivity,0 );  
     }
 
 
@@ -34,8 +36,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate(){
         rb.linearVelocity = new Vector3( _moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed, _moveDirection.z * jumpheight);
-        Cameratrans.localRotation = Quaternion.Euler(0, 0, _facingDirection.y*moussensetivity); 
-        transform.localRotation = Quaternion.Euler(0, _facingDirection.x*moussensetivity,0 );  
+        
     }
     // Update is called once per frame
     
